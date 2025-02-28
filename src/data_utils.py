7046a46015b09048c09898764d501a0cbb782e07
Payal Mitra <payalmitra@Payals-MacBook-Pro.local>
2025-02-28 17:56:37 +0100

    refactor utils

--- src/data_utils.py
+++ src/data_utils.py
@@ -3,9 +3,11 @@ from strenum import LowercaseStrEnum
 from pydantic import BaseModel
 import datetime
 import pandas as pd
+import uuid
 
 
 class COLUMN_NAMES(LowercaseStrEnum):
+    UUID = auto()
     MEAL_ID = auto()
     DATE = auto()
     TIME = auto()
@@ -16,6 +18,7 @@ class COLUMN_NAMES(LowercaseStrEnum):
     FAT = auto()
     CALORIES = auto()
     DAILY_MEAL_ID = auto()
+    USER_ID = auto()
 
 
 class MealRecord(BaseModel):
@@ -35,16 +38,38 @@ def validate_dataframe(df: pd.DataFrame):
     records = df.to_dict(orient='records')
     validated_records = [MealRecord(**record) for record in records]
     return validated_records
-
 # validated_data = validate_dataframe(df)
 
-def postprocess_df(df:pd.DataFrame):
+
+def process_df_for_writing_to_sql_db(df:pd.DataFrame):
     df[COLUMN_NAMES.DATE] = pd.to_datetime(df[COLUMN_NAMES.DATE], format="%Y-%m-%d")
-    #df[COLUMN_NAMES.DATE] = df[COLUMN_NAMES.DATE].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").date)
+    df[COLUMN_NAMES.DATE] = df[COLUMN_NAMES.DATE].dt.date
+    NUTRIENT_COLS = [COLUMN_NAMES.PROTEIN, COLUMN_NAMES.FAT, COLUMN_NAMES.CARBOHYDRATES, COLUMN_NAMES.CALORIES]
+    df[NUTRIENT_COLS] = df[NUTRIENT_COLS].astype(int)
     df[COLUMN_NAMES.DAILY_MEAL_ID] = df.groupby(COLUMN_NAMES.DATE).cumcount() + 1
     df[COLUMN_NAMES.MEAL_ID] = df.apply(lambda x: mint_meal_id(x), axis=1)
+    df[COLUMN_NAMES.UUID] = df.apply(lambda x: generate_uuid(), axis=1)
+    df[COLUMN_NAMES.DATE] = df[COLUMN_NAMES.DATE].apply(lambda x: datetime.datetime.strftime(x, "%Y-%m-%d"))
+
     return df
 
 
+def process_df_for_analysis(df:pd.DataFrame):
+    df[COLUMN_NAMES.DATE] = pd.to_datetime(df[COLUMN_NAMES.DATE], format="%Y-%m-%d")
+    df[COLUMN_NAMES.DATE] = df[COLUMN_NAMES.DATE].dt.date
+    # df[COLUMN_NAMES.DATE] = df[COLUMN_NAMES.DATE].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").date)
+    NUTRIENT_COLS = [COLUMN_NAMES.PROTEIN, COLUMN_NAMES.FAT, COLUMN_NAMES.CARBOHYDRATES, COLUMN_NAMES.CALORIES]
+    df[NUTRIENT_COLS] = df[NUTRIENT_COLS].astype(int)
+    return df
+
 def mint_meal_id(row):
     return "_".join([row[COLUMN_NAMES.DATE].strftime("%Y%m%d"), str(row[COLUMN_NAMES.DAILY_MEAL_ID])])
+
+
+# Function to generate UUID
+def generate_uuid():
+    return str(uuid.uuid4())
+
+# Create an empty DataFrame
+df = pd.DataFrame(columns=['id', 'data'])
+
--- src/llm_utils.py
+++ src/llm_utils.py
@@ -1,6 +1,9 @@
+import datetime
 import os
+from typing import Union
 from dotenv import load_dotenv
 from strenum import LowercaseStrEnum
+import time
 
 from google.genai.types import Tool, GenerateContentConfig, GoogleSearch
 from langchain_google_genai import ChatGoogleGenerativeAI
@@ -8,7 +11,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser, BaseLLMOutputParser
 from langchain_core.prompt_values import PromptValue
+from langchain.globals import set_llm_cache
+# from langchain.cache import InMemoryCache
+from langchain_community.cache import InMemoryCache
 
+set_llm_cache(InMemoryCache())
 
 # do you need ChatMessage from langchain? Or pydantic
 # todo: shift out google specific stuff. This should have more generic code that can wrap around openai, gemini, etc
@@ -27,13 +34,13 @@ DEFAULT_MODEL = ModelId.GEMINI_1_5_FLASH
 
 NUTRI_PROMPT_TEMPLATE_STRING = ("You are a search assistant. The human user will give you a description of their meals, portion size and the time they consumed it. Your task is to analyse the meals, log the date and time of each meal,"
     "and then decompose the meal and its ingredients into their nutrient content. Use google search tool to get nutrient data from https://www.fatsecret.com/calories-nutrition/ if possible. MAke sensible guesses about portions where user does not provide"
-    "Output the amount of carbohydrates, fats, proteins in grams, and calories. "
-    "Please give a unique id to each meal, that is repeated across its components."
+    "Output the amount of carbohydrates, fats, proteins in grams, and calories in integer without making unit explicit"
+    # "Please give a unique id to each meal, that is repeated across its components."
     "Answer with the nutritional breakdown of the meal in a json format (and nothing else) as "
     "[\{{\'Column1': 'Value1', 'Column2': 'Value2', 'Column3': 'Value3'\}},"
     "\{{\'Column1': 'Value4', 'Column2': 'Value5', 'Column3': 'Value6'\}}]"
     "The following are the key/column names, and you must extract the value from the meal analysis"
-    "- meal_id"
+    #"- meal_id"
     "- date"
     "- time"
     "- meal_component"
@@ -51,20 +58,24 @@ SAMPLE_MEAL_DESCRIPTION_STRING = ("Today is 2025-02-25. At noon I ate 1 sandwich
 
 def prepare_prompt(prompt_template_string: str = NUTRI_PROMPT_TEMPLATE_STRING,
                    meal_description_string: str = SAMPLE_MEAL_DESCRIPTION_STRING,
+                   system_date: Union[None, str, datetime.datetime.date] = None,
                    parser: BaseLLMOutputParser = JsonOutputParser) -> PromptValue:
 
     prompt_template = PromptTemplate(
-        template=prompt_template_string + "{format_instructions}" + "\n{meal_description_string}\n",
-        input_variables=["meal_description_string"],
+        template=prompt_template_string + "{format_instructions}" + "\n{meal_description_string}\n"
+        + f" If and only if the user is not specific about the date, then instead use the knowledge that the system logged date is {system_date}",
+        input_variables=["meal_description_string", "system_date"],
         partial_variables={"format_instructions": parser.get_format_instructions()},
     )
     # Generate the prompt
-    filled_prompt = prompt_template.invoke({"meal_description_string": meal_description_string})
+    filled_prompt = prompt_template.invoke({"meal_description_string": meal_description_string,
+                                            "system_date": system_date})
 
     return filled_prompt
 
 
-def prompt_llm_for_response(user_input_string: str,
+def prompt_llm_for_response(user_input_meal_description_string: str,
+                            user_input_date: Union[None, datetime.datetime] = None,
                             model = DEFAULT_MODEL,
                             api_key = GOOGLE_API_KEY):
     # Define custom GenerateContentConfig parameters
@@ -77,7 +88,8 @@ def prompt_llm_for_response(user_input_string: str,
     }
 
     parser = JsonOutputParser()
-    filled_prompt = prepare_prompt(meal_description_string=user_input_string,
+    filled_prompt = prepare_prompt(meal_description_string=user_input_meal_description_string,
+                                   system_date = user_input_date,
                                    parser=parser,
                                    prompt_template_string=NUTRI_PROMPT_TEMPLATE_STRING
                                    )
@@ -86,6 +98,7 @@ def prompt_llm_for_response(user_input_string: str,
         google_search=GoogleSearch()
     )
 
+    start_time = time.time()
     # Initialize ChatGoogleGenerativeAI with client options
     llm = ChatGoogleGenerativeAI(
         model = model,
@@ -95,15 +108,15 @@ def prompt_llm_for_response(user_input_string: str,
             response_modalities=["TEXT"]
         ),
         **client_options,
-
     )
     # client_options =  {'generate_content_config': {'max_output_tokens': 100}})
 
     # Invoke the model with a prompt
     response = llm.invoke(filled_prompt.text)
 
-    print(response.content)
-
+    # print(response.content)
+    end_time = time.time()
+    print(f"Execution time duration: {end_time - start_time} ")
     parsed_json = parser.parse(response.content)
     return parsed_json
 
